796e6ace0fd75f5f811b51435bc10b4e6722460a
Angela <angelarowe7@gmail.com>
2019-04-29 22:03:10 -0400

    Maven and Jacoco addition
    Added Maven framework and Jacoco

--- src/main/java/BabySitterWageCalculator.java
+++ src/main/java/BabySitterWageCalculator.java
@@ -1,5 +1,3 @@
-package main;
-
 public class BabySitterWageCalculator {
 
 	int startTime;
--- src/test/java/BabysitterWageCalculatorTest.java
+++ src/test/java/BabysitterWageCalculatorTest.java
@@ -1,12 +1,11 @@
-package test;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Assert;
-import org.junit.Test;
-
-import main.BabySitterWageCalculator;
-
+@RunWith(JUnit4.class)
 public class BabysitterWageCalculatorTest {
 
 	@Test
